fcb7d3f6857935ad1954af7c1ab4ee03fbe4c0d0
Jasurbek <jasurisoqov685@gmail.com>
2022-06-18 10:18:14 +0500

    main page finished style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+
+import Main from "./Layouts/Main/main";
+
+import "./Assets/Style/main.css";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 };
 
 export default App;
--- src/Layouts/Main/main.js
+++ src/Layouts/Main/main.js
@@ -1,11 +1,17 @@
-const mainPage = () => {
+import React from "react";
+
+/* sytga kirilganda ochiladigan page */
+
+const Main = () => {
   return (
     <div className="main">
       <h1 className="title">
         Welcome to the <span className="covid-19">covid-19</span> statistics
-        website
+        website <span>!</span>
       </h1>
       <button className="come">Come in</button>
     </div>
   );
 };
+
+export default Main;
