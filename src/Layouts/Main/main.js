2a75c5211d22a9ce8b4056225453183d8dfab314
Jasurbek <jasurisoqov685@gmail.com>
2022-06-18 09:41:10 +0500

    main page jsx

---
+++ src/Layouts/Main/main.js
@@ -0,0 +1,11 @@
+const mainPage = () => {
+  return (
+    <div className="main">
+      <h1 className="title">
+        Welcome to the <span className="covid-19">covid-19</span> statistics
+        website
+      </h1>
+      <button className="come">Come in</button>
+    </div>
+  );
+};
