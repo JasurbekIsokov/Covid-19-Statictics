d4b7ce2e3d12727534ab03dbc5f04dda9820d9b4
Jasurbek <jasurisoqov685@gmail.com>
2022-06-18 12:27:04 +0500

    renderRegionListHtml

--- src/Layouts/Main/main.js
+++ src/Layouts/Main/main.js
@@ -1,18 +1,29 @@
 import React from "react";
 
-/* sytga kirilganda ochiladigan page */
+/* saytga kirilganda ochiladigan page */
 
 const main = (props) => {
-  let a = { ...props.regions[0]?.data.data };
-  // console.log(props.regions[0]?.data);
-  console.log(a);
-  props.regions[0]?.data.data.map((val) => {
-    console.log(val);
-  });
+  // props bu yerda davlatlarni olib keladigan obect
+
+  const test = props.regions.length ? "none" : "block";
+
+  const renderRegionListHtml = () => {
+    if (!props.regions.length) return;
+
+    return props.regions[0]?.data.data.map((val) => {
+      console.log(val);
+      return (
+        <div id={val.iso} key={val.iso}>
+          <h2>{val.name}</h2>
+          <h3>{val.iso}</h3>
+        </div>
+      );
+    });
+  };
 
   return (
     <>
-      <div className="main">
+      <div className="main" style={{ display: test }}>
         <h1 className="title">
           Welcome to the <span className="covid-19">covid-19</span> statistics
           website <span>!</span>
@@ -26,6 +37,7 @@ const main = (props) => {
           Come in
         </button>
       </div>
+      {renderRegionListHtml()}
     </>
   );
 };
